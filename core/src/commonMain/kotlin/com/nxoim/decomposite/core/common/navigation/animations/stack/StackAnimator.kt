6212a899f96d4b56eeae647c5b5553a681d9dff9
nxoim <ghite@nxoim.com>
2024-11-04 18:44:44 +0100

    StackAnimator: block inputs on irrelevant items by default

--- core/src/commonMain/kotlin/com/nxoim/decomposite/core/common/navigation/animations/stack/StackAnimator.kt
+++ core/src/commonMain/kotlin/com/nxoim/decomposite/core/common/navigation/animations/stack/StackAnimator.kt
@@ -12,6 +12,7 @@ import androidx.compose.animation.core.Transition
 import androidx.compose.animation.core.createChildTransition
 import androidx.compose.animation.core.rememberTransition
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.IntrinsicMeasurable
 import androidx.compose.ui.layout.IntrinsicMeasureScope
 import androidx.compose.ui.layout.Layout
@@ -47,212 +49,226 @@ import com.nxoim.decomposite.core.common.navigation.animations.ItemLocation.Comp
  */
 @Composable
 fun <Key : Any, Instance : Any> StackAnimator(
-	stackAnimatorScope: StackAnimatorScope<Key, Instance>,
-	modifier: Modifier = Modifier,
-	content: @Composable AnimatedVisibilityScope.(child: Instance) -> Unit,
-	// a key is needed because of something related
-	// to generation of composition hash keys (idk). animation data registry
-	// is expected to be a retained instance, so if theres an accidental
-	// recomposition affecting the instance of the stack animator scope
-	// - the stack animator instance won't be affected since the animation
-	// data registry instance will always stay the same
+    stackAnimatorScope: StackAnimatorScope<Key, Instance>,
+    modifier: Modifier = Modifier,
+    blockInputForNonTopItems: Boolean = true,
+    content: @Composable AnimatedVisibilityScope.(child: Instance) -> Unit,
+    // a key is needed because of something related
+    // to generation of composition hash keys (idk). animation data registry
+    // is expected to be a retained instance, so if theres an accidental
+    // recomposition affecting the instance of the stack animator scope
+    // - the stack animator instance won't be affected since the animation
+    // data registry instance will always stay the same
 ) = key(stackAnimatorScope.animationDataRegistry) {
-	val holder = rememberSaveableStateHolder()
+    val holder = rememberSaveableStateHolder()
 
-	LaunchedEffect(Unit) { stackAnimatorScope.observeAndUpdateAnimatorData() }
+    LaunchedEffect(Unit) { stackAnimatorScope.observeAndUpdateAnimatorData() }
 
-	if (stackAnimatorScope.visibleCachedChildren.isNotEmpty()) Box(modifier) {
-		val seekableTransitionState = remember {
-			SeekableTransitionState(
-				stackAnimatorScope.itemKey(stackAnimatorScope.sourceStack.last())
-			)
-		}
-		val transition = rememberTransition(seekableTransitionState)
+    if (stackAnimatorScope.visibleCachedChildren.isNotEmpty()) Box(modifier) {
+        val seekableTransitionState = remember {
+            SeekableTransitionState(
+                stackAnimatorScope.itemKey(stackAnimatorScope.sourceStack.last())
+            )
+        }
+        val transition = rememberTransition(seekableTransitionState)
 
-		stackAnimatorScope.visibleCachedChildren.forEach { (childKey, cachedInstance) ->
-			// keys are needed for animations to work. something something
-			// correctness something something control flow
-			key(childKey) {
-				val state by stackAnimatorScope.createStackItemState(childKey)
+        stackAnimatorScope.visibleCachedChildren.forEach { (childKey, cachedInstance) ->
+            // keys are needed for animations to work. something something
+            // correctness something something control flow
+            key(childKey) {
+                val state by stackAnimatorScope.createStackItemState(childKey)
 
-				val animationScopes = state.animationData.scopes()
-				// for seekable transitions
-				val firstAnimData = animationScopes.values.first()
-				val animationStatus = firstAnimData.animationStatus
+                val animationScopes = state.animationData.scopes()
+                // for seekable transitions
+                val firstAnimData = animationScopes.values.first()
+                val animationStatus = firstAnimData.animationStatus
 
-				DisposableEffect(Unit) {
-					onDispose {
-						if (!state.inStack) holder.removeState(childHolderKey(childKey))
-					}
-				}
+                DisposableEffect(Unit) {
+                    onDispose {
+                        if (!state.inStack) holder.removeState(childHolderKey(childKey))
+                    }
+                }
 
-				if (state.allowingAnimation) {
-					LaunchedEffect(animationStatus.animating) {
-						if (animationStatus.location.top && !animationStatus.animating) {
-							seekableTransitionState.snapTo(childKey)
-						}
-					}
+                if (state.allowingAnimation) {
+                    LaunchedEffect(animationStatus.animating) {
+                        if (animationStatus.location.top && !animationStatus.animating) {
+                            seekableTransitionState.snapTo(childKey)
+                        }
+                    }
 
-					LaunchedEffect(animationStatus) {
-						snapshotFlow { firstAnimData.animationProgressForScope }
-							.collect() { progress ->
-								if (animationStatus.fromBackIntoTop) {
-									seekableTransitionState.seekTo(
-										(1f - progress).coerceIn(0f, 1f),
-										childKey
-									)
-								} else if (animationStatus.fromOutsideIntoTop) {
-									seekableTransitionState.seekTo(
-										(1f + progress).coerceIn(0f, 1f),
-										childKey
-									)
-								} else if (animationStatus.run { animationType.passiveCancelling && location.top }) {
-									seekableTransitionState.seekTo((-progress).coerceIn(0f, 1f))
-								}
-							}
-					}
+                    LaunchedEffect(animationStatus) {
+                        snapshotFlow { firstAnimData.animationProgressForScope }
+                            .collect() { progress ->
+                                if (animationStatus.fromBackIntoTop) {
+                                    seekableTransitionState.seekTo(
+                                        (1f - progress).coerceIn(0f, 1f),
+                                        childKey
+                                    )
+                                } else if (animationStatus.fromOutsideIntoTop) {
+                                    seekableTransitionState.seekTo(
+                                        (1f + progress).coerceIn(0f, 1f),
+                                        childKey
+                                    )
+                                } else if (animationStatus.run { animationType.passiveCancelling && location.top }) {
+                                    seekableTransitionState.seekTo((-progress).coerceIn(0f, 1f))
+                                }
+                            }
+                    }
 
-					AnimatedVisibilityScopeProvider(
-						transition,
-						visible = { it == childKey },
-						Modifier
-							.accumulate {
-								remember(animationScopes) {
-									state.animationData.modifiers()
-								}
-							}
-							.zIndex((-state.indexFromTop).toFloat()),
-						displaying = { state.displaying },
-					) {
-						holder.SaveableStateProvider(childHolderKey(childKey)) {
-							content(cachedInstance)
-						}
-					}
-				}
-			}
-		}
-	}
+                    AnimatedVisibilityScopeProvider(
+                        transition,
+                        visible = { it == childKey },
+                        Modifier
+                            .accumulate {
+                                remember(animationScopes) {
+                                    state.animationData.modifiers()
+                                }
+                            }
+                            .zIndex((-state.indexFromTop).toFloat()),
+                        displaying = { state.displaying },
+                    ) {
+                        holder.SaveableStateProvider(childHolderKey(childKey)) {
+                            content(cachedInstance)
+                        }
+
+                        if (state.indexFromTop != 0 && blockInputForNonTopItems) Box(
+                            Modifier
+                                .fillMaxSize()
+                                .pointerInput(Unit) {
+                                    awaitPointerEventScope {
+                                        while (true) {
+                                            val event = awaitPointerEvent()
+                                            event.changes.forEach { it.consume() }
+                                        }
+                                    }
+                                }
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Stable
 @Composable
 private inline fun Modifier.accumulate(modifiers: () -> List<Modifier>) =
-	modifiers().fold(initial = this) { acc, modifier -> acc.then(modifier) }
+    modifiers().fold(initial = this) { acc, modifier -> acc.then(modifier) }
 
 @OptIn(InternalDecomposeApi::class)
 private fun <C : Any> childHolderKey(child: C) =
-	child.hashString() + " StackAnimator SaveableStateHolder"
+    child.hashString() + " StackAnimator SaveableStateHolder"
 
 // This converts Boolean visible to EnterExitState
 @Composable
 private inline fun <T> Transition<T>.targetEnterExit(
-	visible: (T) -> Boolean,
-	targetState: T
+    visible: (T) -> Boolean,
+    targetState: T
 ): EnterExitState = key(this) {
-	if (this.isSeeking) {
-		if (visible(targetState)) {
-			Visible
-		} else {
-			if (visible(this.currentState)) PostExit else PreEnter
-		}
-	} else {
-		val hasBeenVisible = remember { mutableStateOf(false) }
+    if (this.isSeeking) {
+        if (visible(targetState)) {
+            Visible
+        } else {
+            if (visible(this.currentState)) PostExit else PreEnter
+        }
+    } else {
+        val hasBeenVisible = remember { mutableStateOf(false) }
 
-		if (visible(currentState)) hasBeenVisible.value = true
+        if (visible(currentState)) hasBeenVisible.value = true
 
-		if (visible(targetState)) {
-			Visible
-		} else {
-			// If never been visible, visible = false means PreEnter, otherwise PostExit
-			if (hasBeenVisible.value) PostExit else PreEnter
-		}
-	}
+        if (visible(targetState)) {
+            Visible
+        } else {
+            // If never been visible, visible = false means PreEnter, otherwise PostExit
+            if (hasBeenVisible.value) PostExit else PreEnter
+        }
+    }
 }
 
 
 private val Transition<EnterExitState>.exitFinished
-	get() = currentState == PostExit && targetState == PostExit
+    get() = currentState == PostExit && targetState == PostExit
 
 
 private class AnimatedVisibilityScopeImpl(
-	_transition: Transition<EnterExitState>
+    _transition: Transition<EnterExitState>
 ) : AnimatedVisibilityScope {
-	override var transition = _transition
-	val targetSize = mutableStateOf(IntSize.Zero)
+    override var transition = _transition
+    val targetSize = mutableStateOf(IntSize.Zero)
 }
 
 private class AnimatedEnterExitMeasurePolicy(
-	val scope: AnimatedVisibilityScopeImpl
+    val scope: AnimatedVisibilityScopeImpl
 ) : MeasurePolicy {
-	var hasLookaheadOccurred = false
-	override fun MeasureScope.measure(
-		measurables: List<Measurable>,
-		constraints: Constraints
-	): MeasureResult {
-		val placeables = measurables.fastMap { it.measure(constraints) }
-		val maxWidth: Int = placeables.fastMaxBy { it.width }?.width ?: 0
-		val maxHeight = placeables.fastMaxBy { it.height }?.height ?: 0
-		// Position the children.
-		if (isLookingAhead) {
-			hasLookaheadOccurred = true
-			scope.targetSize.value = IntSize(maxWidth, maxHeight)
-		} else if (!hasLookaheadOccurred) {
-			// Not in lookahead scope.
-			scope.targetSize.value = IntSize(maxWidth, maxHeight)
-		}
-		return layout(maxWidth, maxHeight) {
-			placeables.fastForEach {
-				it.place(0, 0)
-			}
-		}
-	}
+    var hasLookaheadOccurred = false
+    override fun MeasureScope.measure(
+        measurables: List<Measurable>,
+        constraints: Constraints
+    ): MeasureResult {
+        val placeables = measurables.fastMap { it.measure(constraints) }
+        val maxWidth: Int = placeables.fastMaxBy { it.width }?.width ?: 0
+        val maxHeight = placeables.fastMaxBy { it.height }?.height ?: 0
+        // Position the children.
+        if (isLookingAhead) {
+            hasLookaheadOccurred = true
+            scope.targetSize.value = IntSize(maxWidth, maxHeight)
+        } else if (!hasLookaheadOccurred) {
+            // Not in lookahead scope.
+            scope.targetSize.value = IntSize(maxWidth, maxHeight)
+        }
+        return layout(maxWidth, maxHeight) {
+            placeables.fastForEach {
+                it.place(0, 0)
+            }
+        }
+    }
 
-	override fun IntrinsicMeasureScope.minIntrinsicWidth(
-		measurables: List<IntrinsicMeasurable>,
-		height: Int
-	) = measurables.fastMaxOfOrNull { it.minIntrinsicWidth(height) } ?: 0
+    override fun IntrinsicMeasureScope.minIntrinsicWidth(
+        measurables: List<IntrinsicMeasurable>,
+        height: Int
+    ) = measurables.fastMaxOfOrNull { it.minIntrinsicWidth(height) } ?: 0
 
-	override fun IntrinsicMeasureScope.minIntrinsicHeight(
-		measurables: List<IntrinsicMeasurable>,
-		width: Int
-	) = measurables.fastMaxOfOrNull { it.minIntrinsicHeight(width) } ?: 0
+    override fun IntrinsicMeasureScope.minIntrinsicHeight(
+        measurables: List<IntrinsicMeasurable>,
+        width: Int
+    ) = measurables.fastMaxOfOrNull { it.minIntrinsicHeight(width) } ?: 0
 
-	override fun IntrinsicMeasureScope.maxIntrinsicWidth(
-		measurables: List<IntrinsicMeasurable>,
-		height: Int
-	) = measurables.fastMaxOfOrNull { it.maxIntrinsicWidth(height) } ?: 0
+    override fun IntrinsicMeasureScope.maxIntrinsicWidth(
+        measurables: List<IntrinsicMeasurable>,
+        height: Int
+    ) = measurables.fastMaxOfOrNull { it.maxIntrinsicWidth(height) } ?: 0
 
-	override fun IntrinsicMeasureScope.maxIntrinsicHeight(
-		measurables: List<IntrinsicMeasurable>,
-		width: Int
-	) = measurables.fastMaxOfOrNull { it.maxIntrinsicHeight(width) } ?: 0
+    override fun IntrinsicMeasureScope.maxIntrinsicHeight(
+        measurables: List<IntrinsicMeasurable>,
+        width: Int
+    ) = measurables.fastMaxOfOrNull { it.maxIntrinsicHeight(width) } ?: 0
 }
 
 @OptIn(InternalAnimationApi::class, ExperimentalTransitionApi::class)
 @Composable
 private fun <T> AnimatedVisibilityScopeProvider(
-	transition: Transition<T>,
-	visible: (T) -> Boolean,
-	modifier: Modifier,
-	displaying: () -> Boolean = {
-		visible(transition.targetState) || visible(transition.currentState) ||
-				transition.isSeeking || transition.hasInitialValueAnimations
-	},
-	content: @Composable() AnimatedVisibilityScope.() -> Unit
+    transition: Transition<T>,
+    visible: (T) -> Boolean,
+    modifier: Modifier,
+    displaying: () -> Boolean = {
+        visible(transition.targetState) || visible(transition.currentState) ||
+                transition.isSeeking || transition.hasInitialValueAnimations
+    },
+    content: @Composable() AnimatedVisibilityScope.() -> Unit
 ) {
-	if (displaying()) {
-		val childTransition = transition.createChildTransition(label = "EnterExitTransition") {
-			transition.targetEnterExit(visible, it)
-		}
+    if (displaying()) {
+        val childTransition = transition.createChildTransition(label = "EnterExitTransition") {
+            transition.targetEnterExit(visible, it)
+        }
 
-		if (!childTransition.exitFinished) {
-			val scope = remember(transition) { AnimatedVisibilityScopeImpl(childTransition) }
+        if (!childTransition.exitFinished) {
+            val scope = remember(transition) { AnimatedVisibilityScopeImpl(childTransition) }
 
-			Layout(
-				content = { scope.content() },
-				modifier = modifier,
-				measurePolicy = remember { AnimatedEnterExitMeasurePolicy(scope) }
-			)
-		}
-	}
+            Layout(
+                content = { scope.content() },
+                modifier = modifier,
+                measurePolicy = remember { AnimatedEnterExitMeasurePolicy(scope) }
+            )
+        }
+    }
 }
